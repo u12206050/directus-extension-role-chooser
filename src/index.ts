8f8af98b9c25cad6049b776a833a33c46978c70d
Gerard <gerardlamo@gmail.com>
2024-08-08 12:36:32 +0200

    Updated for Directus v11

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,20 @@
 import { defineModule } from '@directus/extensions-sdk';
-import { Permission } from '@directus/types';
 import ModuleComponent from './module.vue';
 
 export default defineModule({
-  id: 'role-chooser',
-  name: 'Role Chooser',
-  icon: 'security',
-  routes: [
-    {
-      path: '',
-      component: ModuleComponent,
-    },
-  ],
-  preRegisterCheck(user, permissions: Permission[]) {
-    if (user.role && user.role.admin_access) return true;
+	id: 'role-chooser',
+	name: 'Role Chooser',
+	icon: 'security',
+	routes: [
+		{
+			path: '',
+			component: ModuleComponent,
+		},
+	],
+	preRegisterCheck(user, permissions) {
+		if (user.admin_access) return true;
 
-    return !!permissions.find(
-      permission =>
-        permission.collection === 'directus_users' &&
-        permission.action === 'update' &&
-        permission.fields?.includes('role')
-    );
-  },
+		const { update } = permissions['directus_roles'] || {};
+		return Boolean(update && update.fields && update.fields.includes('role'));
+	},
 });
